fddc7e7e6f937e52b96b0c92d4248cc9f6cf5e3b
lucasnaman <lucas.naman@ibexmercado.com>
2025-04-08 21:27:44 +0200

    client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+
+    stream.write(&[1])?;
+    stream.read(&mut [0; 128])?;
+    Ok(())
 }
