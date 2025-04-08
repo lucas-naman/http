d17bef5493e820430cb57ffe2f4c2d925c1e39b1
lucasnaman <lucas.naman@ibexmercado.com>
2025-04-08 22:29:56 +0200

    server read

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,25 @@
-use std::net::TcpListener;
+use std::io::{Error, Read};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    loop {
-        match listener.accept() {
-            Ok((_socket, addr)) => println!("new client: {addr:?}"),
-            Err(e) => println!("couldn't get client: {e:?}"),
+    // accept connections and process them serially
+    for result in listener.incoming() {
+        match result {
+            Ok(stream) => handle_client(stream),
+            Err(e) => connection_error(e),
         }
     }
+}
+
+fn connection_error(e: Error) {
+    println!("couldn't get client: {e:?}")
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = String::new();
+
+    let n = stream.read_to_string(&mut buffer).expect("coulndt read");
+
+    println!("n: {n}/read: {buffer:?}")
 }
